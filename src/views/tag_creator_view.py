56705794de3044874c7ec9651ff945734561ff8d
Kelison Rosendo da Rocha <kelison.rocha@zitrus.com.br>
2024-02-06 17:05:51 -0300

    feat: implement view for tag creator with http types

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,11 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+class TagCreatorView:
+    def validate_and_create(self, http_request: HttpRequest):
+        body = http_request.body
+        product_code = body["product_code"]
+
+        print(product_code)
+
+        return HttpResponse(status_code=200, body={"hello": "world"})
